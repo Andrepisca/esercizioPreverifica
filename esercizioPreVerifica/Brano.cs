1e6c0ee854fe2259ade641e609a9db4724947277
Andrea Piscaglia <ANDREA.PISCAGLIA@local.ittcesena.it>
2022-11-05 09:06:05 +0100

    versione 0.2: aggiunta di una classe brano

--- esercizioPreVerifica/Brano.cs
+++ esercizioPreVerifica/Brano.cs
@@ -8,5 +8,56 @@ namespace esercizioPreVerifica
 {
     internal class Brano
     {
+        List<string> brano = new List<string>();
+        string titolo;
+        string autore;
+        int durata;
+
+
+
+        public Brano(string titolo, string autore, int durata)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.durata = durata;
+        }
+
+        public string getTitolo()
+        {
+            return titolo;
+        }
+
+        public string getAutore()
+        {
+            return autore;
+        }
+        public int getDurata()
+        {
+            return durata;
+        }
+        public void setTitolo(string titoloInput)
+        {
+            titolo = titoloInput;
+        }
+
+        public void setAutore(string autoreInput)
+        {
+            titolo = autoreInput;
+        }
+
+        public void setDurata(int duratainput)
+        {
+            durata = duratainput;
+        }
+
+        public string toSting()
+        {
+            return "il titolo è: " + titolo + " ,l'autore è: " + autore + " ,la durata è: " + durata;
+        }
+
+        public bool shortSong(int durataInput)
+        {
+            return durata < durataInput;
+        }
     }
 }
